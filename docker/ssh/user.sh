0f407d12ee9ae52cecc79a06be8e8c81d7715344
Seraphim Bogomolov <seraphim.bogomolov@gmail.com>
2020-05-26 00:17:11 +0700

    upd user.sh for ssh_secret dirs

--- docker/ssh/user.sh
+++ docker/ssh/user.sh
@@ -4,4 +4,11 @@ set -e
 useradd -m -d /home/${SSH_USER} -G ssh ${SSH_USER} -s /bin/bash
 echo "${SSH_USER}:${SSH_PASS}" | chpasswd
 
+# todo изменить папку хранения секретов на srv
+sudo -u ${SSH_USER} bash -c \
+  'cd;
+  mkdir secrets;
+  mkdir .ssh && chmod 700 .ssh;
+  touch .ssh/authorized_keys && chmod 600 .ssh/authorized_keys'
+
 exec "$@"
