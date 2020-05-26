bc42c0039d0a63a2d16a462c30ab7b83c2e03e0d
Seraphim Bogomolov <seraphim.bogomolov@gmail.com>
2020-05-27 03:53:26 +0700

    add views for team and secret

--- app/storage/urls.py
+++ app/storage/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('teams/', views.TeamListView.as_view(), name='teams'),
+    path('team/<int:pk>', views.TeamDetailView.as_view(), name='team-detail'),
+    path('my-secrets/', views.MySecretListView.as_view(), name='my-secrets'),
+    path('my-secret/<uuid:pk>', views.MySecretDetailView.as_view(), name='my-secret-detail'),
 ]
--- app/storage/views.py
+++ app/storage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Team, Secret
+from django.views import generic
 
 
 def index(request):
@@ -15,3 +16,25 @@ def index(request):
             'num_secrets': num_secrets,
         },
     )
+
+
+class TeamListView(generic.ListView):
+    model = Team
+    paginate_by = 10
+
+
+class TeamDetailView(generic.DetailView):
+    model = Team
+
+
+class MySecretListView(generic.ListView):
+    model = Secret
+    context_object_name = 'my_secret_list'
+    template_name = 'storage/my_secret_list.html'
+    paginate_by = 10
+
+
+class MySecretDetailView(generic.DetailView):
+    model = Secret
+    context_object_name = 'my_secret'
+    template_name = 'storage/my_secret_detail.html'
