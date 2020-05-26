3194f534323287c16494d25620d2748c676cc57c
Seraphim Bogomolov <seraphim.bogomolov@gmail.com>
2020-05-27 02:48:18 +0700

    add home page

--- app/storage/views.py
+++ app/storage/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Team, Secret
 
-# Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+    num_teams = Team.objects.all().count()
+    num_secrets = Secret.objects.all().count()
+
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_teams': num_teams,
+            'num_secrets': num_secrets,
+        },
+    )
